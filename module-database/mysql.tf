08d2c1b3a80ed9ea8625f57c254a3ebd3d77b851
pod3cohort4dev-boop <podcohort4dev@gmail.com>
2025-12-05 00:20:55 +0100

    Update mysql.tf

--- module-database/mysql.tf
+++ module-database/mysql.tf
@@ -1,7 +1,7 @@
 
 resource "aws_db_subnet_group" "mysql_subnet_group" {
   name       = "${var.environment}-mysql-subnet-group"
-  subnet_ids = var.private_subnet_db_ids
+  subnet_ids = module.vpc.private_subnets
   tags = {
     Name        = "${var.environment}-mysql-subnet-group"
     Environment = var.environment
