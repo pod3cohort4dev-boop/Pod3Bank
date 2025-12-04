28d29c6f5b76994e546c8246ffd834028d3f93b7
Rita <ugwuanyinnenna43@gmail.com>
2025-12-04 16:28:56 +0000

    addon

--- module-eks/addons.tf
+++ module-eks/addons.tf
@@ -41,25 +41,23 @@ provider "helm" {
 resource "helm_release" "nginx_ingress" {
   name       = "nginx-ingress"
   namespace  = "ingress-nginx"
-  chart      = "ingress-nginx"
-  repository = "https://kubernetes.github.io/ingress-nginx"
-  version    = "4.7.1"
 
-  # Do NOT install because it already exists
+  # Dummy chart that does nothing
+  chart      = "noop"
+  repository = "https://charts.helm.sh/incubator"
+  version    = "0.1.0"
+
   create_namespace = false
-  timeout          = 1200
 
-  # This prevents Terraform from updating or reinstalling it
   lifecycle {
     ignore_changes = [
       chart,
       version,
       values,
-      repository,
+      repository
     ]
   }
 
-  # Dummy values so Terraform is satisfied
   values = [
     <<EOF
 controller:
